8897b2325d7c6ebae2929874071c6c955843c916
Dominika <dominikaperkowska@gmail.com>
2020-10-16 11:47:23 -0400

    Updates

---
+++ precache-manifest.2fb75f35cc090f65bec4d5eba9789929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8b32e3a520a7224823c95974b0da7b",
+    "url": "/userDirectory/index.html"
+  },
+  {
+    "revision": "bd44036490afa5b7adc2",
+    "url": "/userDirectory/static/css/main.20ba44de.chunk.css"
+  },
+  {
+    "revision": "92f002a332d7a6636cb4",
+    "url": "/userDirectory/static/js/2.082fafa9.chunk.js"
+  },
+  {
+    "revision": "3cabab53dee4769bc4a8c63b1d32918c",
+    "url": "/userDirectory/static/js/2.082fafa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd44036490afa5b7adc2",
+    "url": "/userDirectory/static/js/main.036b4330.chunk.js"
+  },
+  {
+    "revision": "ae488f393188e94212fd",
+    "url": "/userDirectory/static/js/runtime-main.69c5d15d.js"
+  }
+]);
